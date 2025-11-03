6d076efe130902f8ea640f5d877cbe7f28413140
slogup-dale <dyddn304@gmail.com>
2025-11-03 14:36:28 +0900

    feat: input placeholder 기본값 추가

--- components/atoms/Input/Input.tsx
+++ components/atoms/Input/Input.tsx
@@ -3,6 +3,9 @@
 import { Input as InputComponent } from "@/components/ui/input";
 import { InputProps } from "./Input.type";
 
-export const Input = ({ ...props }: InputProps) => {
-  return <InputComponent {...props} />;
+export const Input = ({
+  placeholder = "placeholder",
+  ...props
+}: InputProps) => {
+  return <InputComponent placeholder={placeholder} {...props} />;
 };
