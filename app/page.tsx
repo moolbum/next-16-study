ae2939925460e6c24fc99e0d433d6ad883ec646a
slogup-dale <dyddn304@gmail.com>
2025-11-10 14:28:46 +0900

    feat: 환경변수 예시 env 파일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1084,6 +1084,8 @@ export default function Home() {
 		setAccordionValues(value);
 	};
 
+	console.log('process.env ::', process.env.NEXT_PUBLIC_STATUS);
+
 	return (
 		<div className="p-4 flex flex-col gap-2">
 			<div className="flex flex-col gap-2 w-full mx-auto">
