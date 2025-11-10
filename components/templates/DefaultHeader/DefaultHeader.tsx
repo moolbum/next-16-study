b49eea9202ebcd47eeb90b0a0bfa3c84a3977d17
slogup-dale <dyddn304@gmail.com>
2025-11-10 10:42:31 +0900

    feat: DefaultHeader 내 스켈레톤 추가

--- components/templates/DefaultHeader/DefaultHeader.tsx
+++ components/templates/DefaultHeader/DefaultHeader.tsx
@@ -1,17 +1,29 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useTheme } from '@/stores/useTheme/useTheme';
 import { Moon, Sun } from 'lucide-react';
 
+import { Skeleton } from '@/components/atoms';
+
 export const DefaultHeader = () => {
 	const { theme, toggleTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
 	return (
 		<header className="flex items-center justify-end gap-2 w-full h-16 px-4 bg-background border-b">
-			{theme === 'light' ? (
-				<Sun onClick={toggleTheme} className="size-8 cursor-pointer" />
+			{mounted ? (
+				theme === 'light' ? (
+					<Sun onClick={toggleTheme} className="size-8 cursor-pointer" />
+				) : (
+					<Moon onClick={toggleTheme} className="size-8 cursor-pointer" />
+				)
 			) : (
-				<Moon onClick={toggleTheme} className="size-8 cursor-pointer" />
+				<Skeleton className="size-8" />
 			)}
 		</header>
 	);
